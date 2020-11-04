494e6be8e98248ead9445c4bfca0d79244a49e53
Ander Duran <anduran17f@gmail.com>
2020-11-04 16:37:45 +0100

    juego4raya, meteFicha(), getters(), printMatrix()

--- src/main/juegos/Juego4raya.java
+++ src/main/juegos/Juego4raya.java
@@ -1,8 +1,7 @@
 package main.juegos;
 
 public class Juego4raya {
-    public static boolean[][] ocupados,ocRojos,ocAzul;
-
+    private static boolean[][] ocupados,ocRojos,ocAzul;
 
     private static final Juego4raya miJuego4raya = new Juego4raya();
     public static Juego4raya getJuego4raya(){
@@ -10,20 +9,55 @@ public class Juego4raya {
     }
 
     public void cargarJuego(){
-        ocupados = new boolean[9][6];
-        ocRojos = new boolean[9][6];
-        ocAzul = new boolean[9][6];
+        ocupados = new boolean[6][9];
+        ocRojos = new boolean[6][9];
+        ocAzul = new boolean[6][9];
+
+    }
+
+    public static boolean meteFicha(int colum){
+        if(!ocupados[0][colum]){//hay hueco
+            for (int i=0;i<6;i++){
+                if(!ocupados[5-i][colum]){
+                    ocupados[5-i][colum]=true;
+                    return true;
+                }
+            }
+        }
+        System.out.println("error columna llena");
+        return false;
+    }
+    public static void edit(int fila, int columna){
+        ocupados[fila][columna] = true;
+    }
+
+    public static boolean[][] getOcupados() {
+        return ocupados;
+    }
+
+    public static boolean[][] getOcAzul() {
+        return ocAzul;
+    }
+
+    public static boolean[][] getOcRojos() {
+        return ocRojos;
+    }
+
+    public static boolean hay4raya(){
+        return false;
     }
 
 
-    public static void meteFicha(int colum){
-        if(!ocupados[0][colum]){//hay hueco
-
-        }else{//columna completa
-            System.out.println("columna completa");
+    //metodos para pruebas
+    public static void printMatrix(boolean[][] mat){
+        for (boolean [] y:mat) {
+            System.out.println();
+            for (boolean x:y) {
+                String val="";
+                if(x) {val="T ";}
+                if(!x) {val="F ";}
+                System.out.print(val);
+            }
         }
     }
-
-
-
 }
